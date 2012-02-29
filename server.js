e3e3869b05e65ac391de734059719bfd0cca4707
Loren Yu <yuloren@gmail.com>
2012-02-28 19:13:07 -0800

    Test commit to test deployment 2nd attempt

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ express = require('express');
 var app = express.createServer();
 
 app.get('/', function(req, res){
-    res.send('Hello World. Testing deployment.');
+    res.send('Hello World. Testing deployment.002');
 });
 
 app.listen(8805);
