d3bff4b575abbddc2094efa74ca830485b0c53c3
Loren Yu <yuloren@gmail.com>
2012-03-10 23:19:57 -0800

    Moved browser test runner to /browser for better clarity and because that seems to be the convention

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.get('/', function(req, res){
 	res.render('index');
 });
 
-app.get('/test', function(req, res) {
-	res.render('test/index');
+app.get('/test/browser', function(req, res) {
+	res.render('test/browser/index');
 });
 
 app.get('/js/renderers/*.js', controllers.RendererController.clientRenderer);
