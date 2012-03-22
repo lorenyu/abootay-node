724fed6be2e6497cf5f0c7e6342c2bf975735448
Loren Yu <yuloren@gmail.com>
2012-03-21 23:53:20 -0700

    added data class on client to fetch data from server.

---
+++ public/js/data.js
@@ -0,0 +1,43 @@
+(function() {
+	var abootay = this.abootay;
+
+	var data = abootay.data = {
+		_cache: {},
+
+		/**
+		 * get(modelType, method, *params, callback)
+		 */
+		get: function(modelType, method) {
+			var params = _.toArray(arguments).slice(2),
+				callback = params.pop(),
+				url = '/json/' + escape(modelType) + '/' + escape(method),
+				data = { params: params },
+				key = url + '?' + $.param(data);
+
+			if (abootay.data._cache[key]) {
+				var result = abootay.data._cache[key];
+				return callback(null, result);
+			}
+
+			$.ajax({
+				url: url,
+				data: data,
+				success: function(result) {
+					abootay.data._cache[key] = result;
+					callback(null, result);
+				}
+			});
+		}
+	};
+	_.extend(data.get, {
+		deck: {
+			names: function(callback) {
+				data.get('deck', 'get-deck-names', callback);
+			},
+			cards: function(deckName, callback) {
+				data.get('card', 'get-cards-for-deck', deckName, callback);
+			}
+		}
+	});
+
+})();
