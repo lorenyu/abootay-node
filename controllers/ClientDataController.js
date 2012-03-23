63ec7001e4cc7fd09dce4e1b2d81413176adeae9
Loren Yu <yuloren@gmail.com>
2012-03-23 09:37:06 -0700

    Fixed typo: renamed serverClientData to serveClientData

--- controllers/ClientDataController.js
+++ controllers/ClientDataController.js
@@ -2,7 +2,7 @@ var ClientDataService = require('../services/ClientDataService'),
 	_s = require('underscore.string');
 
 var ClientDataController = module.exports = {
-	serverClientData: function(req, res) {
+	serveClientData: function(req, res) {
 		var parts = req.params[0].split('/'),
 			modelName = _s.capitalize(_s.camelize(parts[0])),
 			method = _s.camelize(parts[1]),
