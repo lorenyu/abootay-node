9a742d0110f36dd67e7afe8a9a040d88c18969b4
Loren Yu <yuloren@gmail.com>
2012-04-15 00:21:28 -0700

    added google analytics

---
+++ public/js/metrics/google-analytics.js
@@ -0,0 +1,11 @@
+(function() {
+	var _gaq = this._gaq || [];
+
+	_gaq.push(['_setAccount', 'UA-13105739-4']);
+	_gaq.push(['_trackPageview']);
+
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+})();
